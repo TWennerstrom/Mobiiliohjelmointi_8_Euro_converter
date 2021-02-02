db27decb2dedb1a5e34e44a17732e838b7cbaa43
Tom Wennerström <teewee@mbnet.fi>
2021-02-02 20:03:49 +0200

    Euro converter

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert, Image } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
 
 export default function App() {
+  const [rates, setRates] = useState([]);
+  const [amount, setAmount] = useState('');
+  const [currency, setCurrency] = useState('');
+  const [eur, setEur] = useState('');
+
+  const getRates = () => {
+    const url = 'https://api.exchangeratesapi.io/latest';
+    fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => { 
+       setRates(responseJson.rates);
+    })
+    .catch((error) => { 
+      Alert.alert('Error' , error); 
+    }); 
+  }
+
+  useEffect(() => { getRates() }, [] );
+
+  const convert = () => {
+    const eur = Number(amount) / rates[currency];
+    setEur(eur.toFixed(2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Image style={{width: 150, height: 150}}
+          source={require('./euro.png')}/>
+      </View>
+        <View>
+          <Text style={styles.text}>{eur} €</Text>
+        </View>
+          <View style={styles.row}>
+            <TextInput style={styles.input}
+              placeholder={'Amount'}
+              keyboardType='numeric'
+              value={amount}
+              onChangeText={text => setAmount(text)}/>
+            <Picker
+              selectedValue={currency}
+              style={{height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) => {
+                setCurrency(itemValue);
+            }}>
+            {Object.keys(rates).sort().map(key => (<Picker.Item label={key} value={key} key={key} />))}
+            </Picker>
+          </View>
+ <Button onPress={() => convert()} title="Convert" />
     </View>
   );
 }
@@ -17,5 +63,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
   },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  row: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+  input: {
+    fontSize: 16,
+    borderWidth: 1,
+    width: 80,
+    textAlign: 'center',
+  }
 });
